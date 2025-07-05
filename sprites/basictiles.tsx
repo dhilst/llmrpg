008f72c33cd938ccc26f80b991541200504ae081
Daniel Hilst <danielhilst@gmail.com>
2025-07-04 21:13:29 -0300

    Fix sprites

--- sprites/basictiles.tsx
+++ sprites/basictiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="basictiles" tilewidth="16" tileheight="16" tilecount="120" columns="8">
- <image source="../../../Downloads/gamesprites/basictiles.png" trans="898989" width="128" height="240"/>
+ <image source="./basictiles.png"  width="128" height="240"/>
  <tile id="12" probability="0.01"/>
  <tile id="20" probability="0.01"/>
  <tile id="30" probability="0.01"/>
